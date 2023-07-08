363bc14109d770ceddd253962ecd2b0f3aecc3ea
Hady41 <h.hawy@ob-soft.com>
2023-07-08 20:24:07 +0300

    intial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Resto app</title>
+    <body>
+    <h2>Resto app</h2>
+    </body>
+</html>
