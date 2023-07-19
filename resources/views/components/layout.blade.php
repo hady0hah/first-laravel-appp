76e94fec0f98399e1ed5cec2acc5bc239e46bdb8
Hady41 <h.hawy@ob-soft.com>
2023-07-19 21:17:08 +0300

    create necessary components

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,95 @@
+<!doctype html>
+
+<title>Resto App</title>
+<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+
+<style>
+    html {
+        scroll-behavior: smooth;
+    }
+
+    .clamp {
+        display: -webkit-box;
+        -webkit-box-orient: vertical;
+        overflow: hidden;
+    }
+
+    .clamp.one-line {
+        -webkit-line-clamp: 1;
+    }
+</style>
+
+<body style="font-family: Open Sans, sans-serif">
+    <section class="px-6 py-8">
+        <nav class="md:flex md:justify-between md:items-center">
+            <div>
+                <a href="/">
+                    <img src="/images/logo.svg" alt="Resto App Logo" width="165" height="16">
+                </a>
+            </div>
+
+            <div class="mt-8 md:mt-0 flex items-center">
+                @auth
+                    <x-dropdown>
+                        <x-slot name="trigger">
+                            <button class="text-xs font-bold uppercase">
+                                Welcome, {{ auth()->user()->name }}!
+                            </button>
+                        </x-slot>
+
+                        @admin
+                            <x-dropdown-item
+                                href="/admin/posts"
+                                :active="request()->is('admin/posts')"
+                            >
+                                Dashboard
+                            </x-dropdown-item>
+
+                            <x-dropdown-item
+                                href="/admin/posts/create"
+                                :active="request()->is('admin/posts/create')"
+                            >
+                                New Post
+                            </x-dropdown-item>
+                        @endadmin
+
+                        <x-dropdown-item
+                            href="#"
+                            x-data="{}"
+                            @click.prevent="document.querySelector('#logout-form').submit()"
+                        >
+                            Log Out
+                        </x-dropdown-item>
+
+                        <form id="logout-form" method="POST" action="/logout" class="hidden">
+                            @csrf
+                        </form>
+                    </x-dropdown>
+                @else
+
+                    <a href="/login"
+                       class="ml-6 text-xs font-bold uppercase {{ request()->is('login') ? 'text-blue-500' : '' }}">
+                        Log In
+                    </a>
+                @endauth
+            </div>
+        </nav>
+
+        {{ $slot }}
+
+        <footer id="newsletter"
+                class="bg-gray-100 border border-black border-opacity-5 rounded-xl text-center py-16 px-10 mt-16"
+        >
+
+            <div class="mt-10">
+                <div class="relative inline-block mx-auto lg:bg-gray-200 rounded-full">
+                </div>
+            </div>
+        </footer>
+    </section>
+
+    <x-flash/>
+</body>
